193ae677f8b101adbb411be845fd1efa09c1e51e
Jtandoh01 <tandoh24700@gmail.com>
2023-07-23 09:20:49 +0000

    ci script to run builds on github for pushes and pull requests to master branch

--- HomeActivity.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomeActivity = () => {
-  return (
-    <Text>Home</Text>
-  )
-}
-
-export default HomeActivity
--- RestaurantActivity.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RestaurantActivity = () => {
-  return (
-    <Text>Restaurant</Text>
-  )
-}
-
-export default RestaurantActivity
